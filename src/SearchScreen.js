fac52664fc781a91a6be4b9725c3223087ad7a6b
Ekaterina Kozelkova <kate.kozelkova@gmail.com>
2018-01-31 11:55:44 +0500

    Add API artists request

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -6,9 +6,18 @@ class SearchScreen extends React.Component {
     //on click, on enter
     handleSearch = () => {
         let results = ["hello", "kate"];
-        setTimeout(() => {
+
+        fetch('http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=cher&api_key=3195564c027455aa3a38e82fd6dd3e0f&format=json')
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(response) {
+                console.log(response.results.artistmatches.artist)
+            });
+
+        /*setTimeout(() => {
             this.props.updateResults(results)
-        }, 1000)
+        }, 1000)*/
     };
 
     render() {
